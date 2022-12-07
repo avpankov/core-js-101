44b1e1e54c61a696053324ce7c818d13e391c2f4
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-12-07 09:57:42 +0300

    feat: add propagateItemsByPositionIndex() in 03-arrays-tasks.js

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -298,8 +298,15 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  // throw new Error('Not implemented');
+  let result = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < i + 1; j += 1) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
 
 
